4c2eb37dc934fd4fcbbdd162cd79c3e3f2899ebe
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-03 16:03:32 -0600

    Created CreateSweepstakesManager Method in SweepstakesManagerFactory Class. Created string managerType to get userInput picking Stack or Queue manager, using switch case to create and return the specified one or throw an exception.

--- SweepstakesApplication/SweepstakesManagerFactory.cs
+++ SweepstakesApplication/SweepstakesManagerFactory.cs
@@ -8,6 +8,18 @@ namespace SweepstakesApplication
 {
     public static class SweepstakesManagerFactory
     {
-        
+        public static ISweepstakesManager CreateSweepstakesManager()
+        {
+            string managerType = UserInterface.GetUserInputFor("Type below which manager you would like to create: Stack or Queue?");
+            switch (managerType.ToLower())
+            {
+                case "stack":
+                    return new SweepstakesStackManager();
+                case "queue":
+                    return new SweepstakesQueueManager();
+                default:
+                    throw new ApplicationException(string.Format("Not a valid Sweepstakes Manager Type"));
+            }
+        }
     }
 }
